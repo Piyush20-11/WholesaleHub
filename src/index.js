c3f0dc0673b169b608f13d1cd4b6e06b1d38ce78
Piyush20-11 <piyush275202@gmail.com>
2024-02-14 17:28:13 +0530

    Basic Login and Register page Created

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Register from './pages/register/register.jsx';
+import Login from "./pages/login/login.jsx";
+ReactDOM.render(
+    <div>
+        
+        <Login />
+    </div> 
+    ,document.getElementById("root"));
